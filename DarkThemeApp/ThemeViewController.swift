54053c0ef3127fe3ffa244f6e606d9eee9ccb033
Ērika Tālberga <103637112+zanz1bra@users.noreply.github.com>
2023-10-21 14:41:00 +0300

    home task 7
    Did home task, added icon to app

--- DarkThemeApp/ThemeViewController.swift
+++ DarkThemeApp/ThemeViewController.swift
@@ -14,31 +14,48 @@ class ThemeViewController: UIViewController {
     @IBOutlet weak var trashButton: UIBarButtonItem!
 // #warning("need to have IBOutlet another Item button and put on leading side")
     
+    var darkTheme = false
+    var mainText = "Dark Theme Off"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         print("ThemeViewController func viewDidLoad")
         darkButton.layer.cornerRadius = 10
+        darkThemeIsOn(isOn: darkTheme)
     }
 
     @IBAction func darkButtonTapped(_ sender: Any) {
         print("darkButtonTapped")
-        view.backgroundColor = UIColor.black
-        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        darkThemeIsOn(isOn: darkTheme)
+//        view.backgroundColor = UIColor.black
+//        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+//        
+//        darkButton.setTitle("Dark Theme off", for: .normal)
+//        darkButton.setTitleColor(UIColor.black, for: .normal)
+//        darkButton.tintColor = UIColor.white
         
-        darkButton.setTitle("Dark Theme off", for: .normal)
-        darkButton.setTitleColor(UIColor.black, for: .normal)
-        darkButton.tintColor = UIColor.white
-        
-        #warning("Dark Theme off : Dark Theme on")
+//        #warning("Dark Theme off : Dark Theme on")
+    }
+    
+    func darkThemeIsOn(isOn: Bool) {
+        mainText = isOn ? "Dark Theme Off" : "Dark Theme On"
+        darkButton.setTitle(mainText, for: .normal)
+        darkButton.setTitleColor(isOn ? UIColor.black : UIColor.white, for: .normal)
+        navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: !isOn ? UIColor.black : UIColor.white]
+        darkButton.backgroundColor = !isOn ? .black : .white
+        view.backgroundColor = isOn ? UIColor.black : UIColor.white
+        trashButton.tintColor = !isOn ? .black : .white
+        folderButton.tintColor = !isOn ? .black : .white
+        darkTheme.toggle()
     }
     
     @IBAction func folderTapped(_ sender: Any) {
-        basicAlert2(title: "My Folder", message: "This is my item button!")
+        basicAlert(title: "My Folder", message: "This is my item button!")
     }
 //    #warning("Need to have IBAction for Item button and logic to present actionSheet")
     @IBAction func trashButtonTapped(_ sender: Any) {
-        basicAlert(title: "Trash", message: "Are you sure?")
+        sheetAlert(title: "Trash", message: "Are you shure?")
     }
     
 }
@@ -54,12 +71,12 @@ extension UIViewController {
         }
     }
     
-    func basicAlert2(title: String?, message: String?) {
+    func sheetAlert(title: String?, message: String?) {
         DispatchQueue.main.async {
-            let alert2 = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
-            alert2.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
+            let sheetAlert = UIAlertController(title: title, message: message, preferredStyle: .actionSheet)
+            sheetAlert.addAction(UIAlertAction(title: "Yes", style: .default, handler: nil))
             
-            self.present(alert2, animated: true)
+            self.present(sheetAlert, animated: true)
         }
     }
 }
